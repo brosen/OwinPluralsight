dee74a09bcd93273ac4bfb0c78d4c9adbbf57c35
brad rosen <brosen@mdsol.com>
2020-05-08 13:48:03 -0400

    init

---
+++ OwinPluralsight/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OwinPluralsight.MvcApplication" Language="C#" %>
---
+++ OwinPluralsight/Startup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Diagnostics;
+
+[assembly: OwinStartupAttribute(typeof(OwinPluralsight.Startup))]
+namespace OwinPluralsight
+{
+    public partial class Startup
+    {
+        public static string html="<html><body>Hello World</body></html>";
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+
+            //owin contex, next thing in pipeline
+            app.Use(async (ctx, next) => {
+                //debugging middleware
+                Debug.WriteLine("Incoming request: " + ctx.Request.Path);
+                await next(); //wait for pipeline on the response way back
+                Debug.WriteLine("Outgoing request: " + ctx.Request.Path);
+            });
+
+
+            //delegate function below
+            app.Use(async(ctx, next) => {
+                await ctx.Response.WriteAsync(html); //since this is async, dont forget await so that it doesnt return until this is complete
+                //response writes to response body
+            }); 
+        }
+
+
+    }
+}
